e5db8e861d9460eb79360f35c100f865af96c8c4
chejen <jkopre.qek@gmail.com>
2016-03-10 23:29:32 +0800

    use fetch instead of XHR

--- src/ui/actions/TranslationActions.js
+++ src/ui/actions/TranslationActions.js
@@ -3,16 +3,34 @@
 var Reflux = require('reflux');
 var config = require('../../config');
 var host = "http://" + config.server.hostname + ":" + config.server.port;
-
 var TranslationActions = Reflux.createActions({
 	'addTranslation': { children: ['completed', 'failed'] },
 	'loadTranslations': { children: ['completed', 'failed'] },
 	'removeTranslation': { children: ['completed', 'failed'] },
 	'updateTranslation': { children: ['completed', 'failed'] }
 });
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 
 TranslationActions.addTranslation.listen(function(data) {
-	$.ajax({
+	fetch(host + '/api/translation', {
+		headers: {
+			'Accept': 'application/json; charset=utf-8',
+			'Content-Type': 'application/json; charset=utf-8'
+		},
+		method: 'POST',
+		body: JSON.stringify(data)
+	})
+	.then(res => {
+		if (res.status >= 400) {
+			throw new Error(res.status + ", " + res.statusText);
+		}
+		return res.json();
+	})
+	.then(this.completed)
+	.catch(this.failed)
+	/*$.ajax({
 		url: host + '/api/translation',
 		contentType: "application/json; charset=utf-8",
 		type: "POST",
@@ -20,32 +38,67 @@ TranslationActions.addTranslation.listen(function(data) {
 		dataType: "json"
 	})
 	.done(this.completed)
-	.fail(this.failed);
+	.fail(this.failed);*/
 });
 
 TranslationActions.loadTranslations.listen(function() {
-	$.ajax({
+	fetch(host + '/api/translation')
+		.then(res => {
+			if (res.status >= 400) {
+				throw new Error(res.status + ", " + res.statusText);
+			}
+			return res.json();
+		})
+		.then(this.completed)
+		.catch(this.failed)
+	/*$.ajax({
 		url: host + '/api/translation',
 		type: "GET",
 		dataType: "json"
 	})
 	.done(this.completed)
-	.fail(this.failed);
+	.fail(this.failed);*/
 });
 
 TranslationActions.removeTranslation.listen(function(id) {
-	$.ajax({
+	fetch(host + '/api/translation/' + id, {
+		method: 'DELETE'
+	})
+	.then(res => {
+		if (res.status >= 400) {
+			throw new Error(res.status + ", " + res.statusText);
+		}
+		return res.json();
+	})
+	.then(this.completed)
+	.catch(this.failed)
+	/*$.ajax({
 		url: host + '/api/translation/' + id,
 		type: "DELETE",
 		dataType: "json"
 	})
 	.done(this.completed)
-	.fail(this.failed);
+	.fail(this.failed);*/
 });
 
 TranslationActions.updateTranslation.listen(function(data) {
-	console.log("TranslationActions.updateTranslation", data, data._id, data.id);
-	$.ajax({
+	fetch(host + '/api/translation/' + data._id, {
+		headers: {
+			'Accept': 'application/json; charset=utf-8',
+			'Content-Type': 'application/json; charset=utf-8'
+		},
+		method: 'PUT',
+		body: JSON.stringify(data)
+	})
+	.then(res => {
+		if (res.status >= 400) {
+			throw new Error(res.status + ", " + res.statusText);
+		}
+		return res.json();
+	})
+	.then(this.completed)
+	.catch(this.failed)
+	/*$.ajax({
 		url: host + '/api/translation/' + data._id,
 		contentType: "application/json; charset=utf-8",
 		type: "PUT",
@@ -53,7 +106,7 @@ TranslationActions.updateTranslation.listen(function(data) {
 		dataType: "json"
 	})
 	.done(this.completed)
-	.fail(this.failed);
+	.fail(this.failed);*/
 });
 
 module.exports = TranslationActions;
