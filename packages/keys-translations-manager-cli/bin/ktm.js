ac9c3b2aaefa99a8934ce282c124d49a2ab46206
chejen <jkopre.qek@gmail.com>
2016-03-15 00:24:39 +0800

    define cli options

--- packages/keys-translations-manager-cli/bin/ktm.js
+++ packages/keys-translations-manager-cli/bin/ktm.js
@@ -1,5 +1,29 @@
 #!/usr/bin/env node
-var argv = require('yargs').argv;
 var chalk = require('chalk');
+var yargs = require('yargs');
+var argv = yargs.argv;
 
-console.log(chalk.bold.green("output to: ") + argv.dist);
+yargs
+    .usage('Usage: $0 [locale1 (, locale2, ...)] -t [json or properties] -p [project ID] -l -o [output directory]')
+    .example('$0 us-US zh-TW -t json -p p1 -o foo/bar --format')
+    .demand(['t', 'p', 'o'])
+    .boolean('format')
+    .alias('f', 'format')
+    .option('type', {
+        alias: 't',
+        describe: 'Provide a data type',
+        choices: ['json', 'properties']
+    })
+    .option('project', {
+        alias: 'p',
+        describe: 'Provide a project ID'
+    })
+    .option('output', {
+        alias: 'o',
+        describe: 'Provide an output directory'
+    })
+
+    .help('help')
+    .argv
+
+console.log('Output to: ', process.cwd(), chalk.bold.green(argv.output || argv.o), argv, argv.format, argv.f);
