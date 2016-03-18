1a5214b7d41dffb4509d69a435d27650cab173e6
chejen <jkopre.qek@gmail.com>
2016-03-18 23:11:15 +0800

    support the use of placeholder for .ktmrc

--- packages/keys-translations-manager-cli/bin/ktm.js
+++ packages/keys-translations-manager-cli/bin/ktm.js
@@ -7,8 +7,8 @@ var chalk = require('chalk'),
 	cwd = process.cwd(),
 	path = require('path'),
 	parts = cwd.split(path.sep),
-    mongoose = require('mongoose'),
-    Properties2Json = require('../lib/Properties2Json.js'),
+	mongoose = require('mongoose'),
+	Properties2Json = require('../lib/Properties2Json.js'),
 	runcom = ".ktmrc",
 	loc, f, content, cfg;
 
@@ -32,8 +32,7 @@ if (!cfg) {
 yargs
 	.usage('Usage: ktm [locale1 (, locale2, ...)] -t [json|properties] -p [project ID] -o [output directory]')
 	.example('ktm us-US zh-TW -t json -p p1 --format')
-	//.command('download', "Download locales from a specific project.")
-	.demand(['t', 'p'/*, 'o'*/])
+	.demand(['t', 'p'])
 	.option('type', {
 		alias: 't',
 		describe: 'Provide a data type',
@@ -43,105 +42,97 @@ yargs
 		alias: 'p',
 		describe: 'Provide a project ID'
 	})
-	/*.option('output', {
-		alias: 'o',
-		describe: 'Provide an output directory'
-	})*/
 	.boolean('format')
 	.alias('f', 'format')
 	.help('help')
 	.alias('h', 'help')
 	.argv
 
-console.log(argv, argv._);
-console.log(cfg.output.path.replace("${locale}", "en-US"));
-console.log(cfg.output.filename);
-//console.log(path.join(loc, cfg.output.path.replace("${locale}", "en-US"), cfg.output.filename));
 mongoose.connect(cfg.database, function(err) {
 	if (err) {
-        console.log(chalk.red('Failed to connect database !'));
-        console.log(chalk.red(err));
+		console.log(chalk.red('Failed to connect database !'));
+		console.log(chalk.red(err));
 	} else {
-        var locales = argv._,
-            lenLocales = locales.length,
-        	schema = {
-        		'key': String,
-        		'project': [String]
-        	},
-        	TranslationSchema,
-            Translations,
-            count = 0;
+		var locales = argv._,
+			lenLocales = locales.length,
+			schema = {
+				'key': String,
+				'project': [String]
+			},
+			TranslationSchema,
+			Translations,
+			count = 0;
 
-        while(lenLocales--) schema[locales[lenLocales]] = String;
-        TranslationSchema = new mongoose.Schema(schema);
-        Translations = mongoose.model('Translation', TranslationSchema);
+		while(lenLocales--) schema[locales[lenLocales]] = String;
+		TranslationSchema = new mongoose.Schema(schema);
+		Translations = mongoose.model('Translation', TranslationSchema);
 
 
-        locales.forEach(function(locale){
-            var filePath = path.join(loc, cfg.output.path.replace("${locale}", locale)),
-				file = path.join(filePath, cfg.output.filename),
-                outputType = argv.format ? 'f' : '',
-                fileType = argv.t || argv.type,
-                project = argv.p || argv.project,
-                query,
-                criteria = {
-                    "project": project
-                },
-                select = {
-                    "_id": 0,
-                    "key": 1
-                };
+		locales.forEach(function(locale){
+			var outputType = argv.format ? 'f' : '',
+				fileType = argv.t || argv.type,
+				project = argv.p || argv.project,
+				filePath = path.join(loc, cfg.output.path.replace("${locale}", locale)),
+				file = path.join(filePath, cfg.output.filename.replace("${locale}", locale) + `.${fileType}`),
+				query,
+				criteria = {
+					"project": project
+				},
+				select = {
+					"_id": 0,
+					"key": 1
+				};
 
-            criteria[locale] = {$exists: true};
-            select[locale] = 1;
-            query = Translations.find(criteria).select(select);
-            if (outputType === "f") query.sort({'key':-1});
-            query.exec(function(err, translations) {
-                var len,
-                    translation,
-                    rootObj = {},
-                    str = "";
+			criteria[locale] = {$exists: true};
+			select[locale] = 1;
+			query = Translations.find(criteria).select(select);
+			if (outputType === "f") query.sort({'key':-1});
+			query.exec(function(err, translations) {
+				var len,
+					translation,
+					rootObj = {},
+					str = "";
 
-                if (err) {
-                    mongoose.connection.close();
-                    console.log(chalk.red(err));
-                    return;
-                }
+				if (err) {
+					mongoose.connection.close();
+					console.log(chalk.red(err));
+					return;
+				}
 
-                len = translations.length;
+				len = translations.length;
 
-                if (fileType === "json") {
-                    while(len--) {
-                        translation = translations[len];
-                        Properties2Json(rootObj, translation.key, translation[locale]);
-                    }
+				if (fileType === "json") {
+					while(len--) {
+						translation = translations[len];
+						Properties2Json(rootObj, translation.key, translation[locale]);
+					}
 
-                    if (outputType === "f") { //formatted
-                        str = JSON.stringify(rootObj, null, 2)
-                    } else { //minimized
-                        str = JSON.stringify(rootObj);
-                    }
+					if (outputType === "f") { //formatted
+						str = JSON.stringify(rootObj, null, 2)
+					} else { //minimized
+						str = JSON.stringify(rootObj);
+					}
 
-                } else if (fileType === "properties") {
-                    while(len--) {
-                        translation = translations[len];
-                        str += translation.key + "=" + translation[locale] + "\r\n";
-                    }
-                }
+				} else if (fileType === "properties") {
+					while(len--) {
+						translation = translations[len];
+						str += translation.key + "=" + translation[locale] + "\r\n";
+					}
+				}
 
-                fs.writeFile(file, str, function (err) {
-                    if (err) {
-                        if (err.code === 'ENOENT'){
+				fs.writeFile(file, str, function (err) {
+					if (err) {
+						if (err.code === 'ENOENT'){
 							fs.mkdir(filePath, 0777, true, function (err) {
 								if (err) {
 									mongoose.connection.close();
-			                        console.log(chalk.red(err));
+									console.log(chalk.red(err));
 								} else {
 									console.log(chalk.green(filePath + ' created.'));
 									fs.writeFile(file, str, function (err) {
 										if (err) {
 											mongoose.connection.close();
-					                        console.log(chalk.red(err));
+											console.log(chalk.red(err));
 										} else {
 											console.log(chalk.green('Successfully output to: ' + file));
 										}
@@ -150,18 +141,18 @@ mongoose.connect(cfg.database, function(err) {
 							});
 						} else {
 							mongoose.connection.close();
-	                        console.log(chalk.red(err));
+							console.log(chalk.red(err));
 						}
-                    } else {
-                    	console.log(chalk.green('Successfully output to: ' + file));
+					} else {
+						console.log(chalk.green('Successfully output to: ' + file));
 					}
-                });
+				});
 
-                if (++count === locales.length) {
-                    mongoose.connection.close();
-                }
-            });
-        });
+				if (++count === locales.length) {
+					mongoose.connection.close();
+				}
+			});
+		});
 
-    }
+	}
 });
