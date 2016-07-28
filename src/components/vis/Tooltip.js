03fe11c8cd570e3800a77ab2564c4d9d9bc50c48
chejen <jkopre.qek@gmail.com>
2016-07-28 23:56:21 +0800

    added Tooltip component

---
+++ src/components/vis/Tooltip.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import timingUtil from 'keys-translations-manager-core/lib/timingUtil'
+
+export default class Tooltip extends React.Component {
+	static propTypes = {
+		children: React.PropTypes.node,
+		style: React.PropTypes.object.isRequired
+	};
+
+	constructor() {
+		super();
+		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+		this.state = {
+			"display": "none",
+			"top": 0,
+			"left": 0
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const { display, top, left } = nextProps;
+		this.setState({
+			display, top, left
+		});
+	}
+
+	onMouseOver() {
+		clearInterval(timingUtil.getTimeoutId());
+/* 		this.setState({
+			display: "inline"
+		}); */
+	}
+
+	onMouseOut() {
+		const me = this;
+		const timeoutId = setTimeout(() => {
+			me.setState({
+				display: "none"
+			});
+		}, 200);
+		timingUtil.setTimeoutId(timeoutId);
+	}
+
+	render() {
+		const { display, top, left } = this.state;
+		const style = { display, top, left };
+		return (
+			<span className="app-tooltip" style={style}
+					onMouseOver={this.onMouseOver.bind(this)}
+					onMouseOut={this.onMouseOut.bind(this)}>
+				{this.props.children}
+			</span>
+		);
+	}
+}
